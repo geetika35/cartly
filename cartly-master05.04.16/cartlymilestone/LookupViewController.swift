251af3779fcf36d86570e482401e90aea56c2951
gtandrews1 <samsamthekat@gmail.com>
2016-05-05 09:59:51 -0500

    changes 5/4/16
    added nav bar and menu button to the map view. They don't seem to center in the nav bar object when displaying in the simulator though...

---
+++ cartly-master05.04.16/cartlymilestone/LookupViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LookupViewController.swift
+//  cartlymilestone
+//
+//  Created by Geetika Batra on 4/25/16.
+//  Copyright © 2016 Geetika Batra. All rights reserved.
+//
+
+import Foundation
---
+++ cartly-master05.04.16/cartlymilestone/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cartlymilestone
+//
+//  Created by Geetika Batra on 3/30/16.
+//  Copyright © 2016 Geetika Batra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func mainmenu(sender: AnyObject) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
